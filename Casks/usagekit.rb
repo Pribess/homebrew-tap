3907531bd26fadb38c3ad3307bece3e20a144c07
Anka <runner@sjc22-bt147-00690647-2fa4-4b4d-a447-f1ff836ffed2-BE72EBD69233.local>
2026-03-19 15:29:06 +0000

    Update UsageKit to 0.0.3

--- Casks/usagekit.rb
+++ Casks/usagekit.rb
@@ -1,6 +1,6 @@
 cask "usagekit" do
-  version "0.0.2"
-  sha256 "96b899839dd1f2c7408ed71042b6fe5dc37e9fd7eae6dd5a012f7c25d28945f8"
+  version "0.0.3"
+  sha256 "938c1b59eeba798a38aa3b0f4d688887cf57acf58e61f53fbb9a4d37b2ce3f1c"
 
   url "https://github.com/Pribess/UsageKit/releases/download/v#{version}/UsageKit.dmg"
   name "UsageKit"
